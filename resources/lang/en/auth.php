f69f60f71b6bb4f5e5705932be493fa4b51e26e5
Ross Summerell <ross@LSDomain.local>
2019-10-30 21:23:21 +0200

    Login & Mail set up

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -1,8 +1,8 @@
-<?php
+ <?php
 
-return [
+    return [
 
-    /*
+        /*
     |--------------------------------------------------------------------------
     | Authentication Language Lines
     |--------------------------------------------------------------------------
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+        'failed' => 'These credentials do not match our records.',
+        'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
-];
+    ];
